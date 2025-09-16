e816035db2c85303cbb22a7b058549870f85880a
NairaaOfficial <nairaofficial2000@gmail.com>
2025-09-16 23:05:56 +0530

    fixed scaling issues.

--- video_enhancer_quality/video_enhancer.py
+++ video_enhancer_quality/video_enhancer.py
@@ -137,7 +137,7 @@ def process_video(input_video_path, reel_number):
 
         fps = cap.get(cv2.CAP_PROP_FPS)
         x, y, w, h = detected_x, detected_y, detected_w, detected_h
-        out = cv2.VideoWriter(cropped_video_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+        out = cv2.VideoWriter(cropped_video_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w * 4, h * 4))
         total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
         print(f"Total frames to process: {total_frames}")
         count = 0
